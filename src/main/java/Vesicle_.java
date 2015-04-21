6c1bf7688fbfafb7bbcd56f068b8f09cd9075fa6
Max Larsson <max.larsson@gmail.com>
2015-04-21 15:00:00 +0200

    Bumped version.

--- src/main/java/Vesicle_.java
+++ src/main/java/Vesicle_.java
@@ -27,10 +27,10 @@ import ij.measure.*;
 interface Version {
     String title = "Vesicle";
     String author = "Max Larsson";
-    String version = "1.0pre1";
-    String year = "2014";
-    String month = "November";
-    String day = "11";
+    String version = "1.0pre2";
+    String year = "2015";
+    String month = "April";
+    String day = "21";
     String email = "max.larsson@liu.se";
     String homepage = "http://www.hu.liu.se/forskning/larsson-max/software";
 }
